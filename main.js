fc7e78a0c0bd471d57d46e78692ade9d149ddf5c
amog1 <160067063+amog1@users.noreply.github.com>
2024-05-16 13:55:13 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+// event listner
+document.getElementById("markquiz").addEventListener("click", mark);
+
+// html variavles
+let queOne = document.getElementById("king").toLowercase();
+let queTwo = document.getElementById("queen").toLowercase();
+let queThree = document.getElementById("squares").toLowercase();
+let queFour = document.getElementById("any").toLowercase();
+// function
+function mark() {}
